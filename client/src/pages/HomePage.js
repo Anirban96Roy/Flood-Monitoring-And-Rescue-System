9dfea2c3c0271a3b3f5f3505992e92084f1c22b4
Anirban96Roy <roy778081@gmail.com>
2024-10-25 13:47:36 +0600

    Homepage 80% done

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,21 +5,29 @@ import v from '../components/v.mp4';
 const HomePage = () => {
   return (
     <Layout>
-       <div class="content">
-        <div class="overlay"></div>
-        <video autoPlay loop muted plays-inline class="bgvideo">
-        <source src={v} autoPlay loop muted />
-        </video>
-        <h1>Flood Management & Monitoring System</h1>
-        <p> Welcome to our Flood Management & Monitoring System, designed to provide real-time data and resources to help communities prepare for and respond to flooding events.<br></br>Our platform utilizes advanced technology to track weather patterns, monitor water levels, and provide timely alerts.<br></br> We aim to enhance safety and resilience by empowering individuals and organizations with the information they need to mitigate the impact of floods. Join us in our mission to create safer, more prepared communities!</p>
-        
-        <div>
-          <button type="button">Need Help?</button>
-          <button type="button">Wanna Be a Volunteer?</button>
-       </div>
-
-       </div>
-    </Layout>
+            <div className="content">
+                <video autoPlay loop muted playsInline className="bgvideo">
+                    <source src={v} type="video/mp4" />
+                </video>
+                <div className="overlay"></div> {/* Dark overlay on video */}
+                <h1>Flood Management & Monitoring System</h1>
+                <p>
+                    Welcome to our Flood Management & Monitoring System, designed to provide real-time data and resources to help communities prepare for and respond to flooding events.<br />
+                    Our platform utilizes advanced technology to track weather patterns, monitor water levels, and provide timely alerts.<br />
+                    We aim to enhance safety and resilience by empowering individuals and organizations with the information they need to mitigate the impact of floods. Join us in our mission to create safer, more prepared communities!
+                </p>
+                <div className="buttonx">
+                    <button type="button">
+                        Need Help?
+                        <span></span> {/* This span is for the hover effect */}
+                    </button>
+                    <button type="button">
+                        Wanna Be a Volunteer?
+                        <span></span> {/* This span is for the hover effect */}
+                    </button>
+                </div>
+            </div>
+        </Layout>
   )
 }
 
